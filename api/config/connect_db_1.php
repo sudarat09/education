d64083c381653da36657c52bf887e1fc47c86053
sudarat09~ <tartar09@gmail.com>
2022-03-18 17:35:46 +0700

    add utf8

--- api/config/connect_db_1.php
+++ api/config/connect_db_1.php
@@ -1,4 +1,13 @@
 <?php
+
+// server
+// $servername = "us-cdbr-east-05.cleardb.net";
+// $username = "b75b79dccf2c1c";
+// $password = "cc746e84";
+// $database="heroku_67a8f7daac85d18";
+
+
+// // local
 $servername = "localhost";
 $username = "root";
 $password = "12345678";
@@ -6,6 +15,9 @@ $database="addmember";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$database);
+
+// Change character set to utf8
+mysqli_set_charset($conn,"utf8");
 
 // Check connection
 if (!$conn) {
